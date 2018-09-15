9ba7823aac69a715031e3590a1701cc6109852e8
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-15 22:58:02 +0530

    basic communication is working and understood functionality

--- server_old.js
+++ server_old.js
@@ -8,17 +8,21 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
 
-    socket.on('connect', (msg) => {
-        console.log(msg);
-        io.emit('connect', 'Welcome to Socket' + msg)
-    });
 
-
+    // recived message and send back to client on same socket
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
-        io.emit('chat message', msg.length+ 'edit');
-        io.emit('connect', 'OK From Server')
+        io.emit('chat message', msg + ' ----------->client to server and all');
     });
+
+    // recived message and send back to client on same socket with new date
+    socket.on('chat from server', (msg) => {
+        console.log('message for server: ' + msg);
+        if (msg.length > 0) {
+            io.emit('chat from server', 'Message Recd and accepted: ' + msg + Date.now()+ 1)           
+        }
+
+    })
     socket.on('disconnect', function () {
         console.log('user disconnected');
         io.emit('disconnect', 'user disconnected')
